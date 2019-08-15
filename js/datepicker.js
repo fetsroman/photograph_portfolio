56e8715db5bc1cb6c58d2a16f102352813926c85
roman <fets.roman@gmail.com>
2019-08-15 16:41:07 +0300

    New styling

--- js/datepicker.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('#datepicker').datepicker({
-    inline: true,
-    showOtherMonths: true,
-    dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-  });
-});
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,23 +1,35 @@
 $(document).ready(function() {
   // Gets the video src from the data-src on each button
-  var vimeoVideoID = '265045525';
   
-  $('.video-btn').each(function() {
-    src = $(this).data( "src" );
-    arr = src.split('/')
-    $vimeoVideoID = arr.slice(-1)
-  });
+  // $('.video-btn').each(function() {
+  //   $src = $(this).data( "src" );
+  //   arr = $src.split('/');
+  //   $vimeoVideoID = arr.slice(-1);
+    
+  //   $.getJSON('https://www.vimeo.com/api/v2/video/' + $vimeoVideoID + '.json?callback=?', {format: "json"}, function(data) {
 
-  $.getJSON('https://www.vimeo.com/api/v2/video/' + $vimeoVideoID + '.json?callback=?', {format: "json"}, function(data) {  
-    $(".thumbs").attr('src', data[0].thumbnail_large);
-  });
+  //   $('.video-btn').each(function() {
+  //       $src = $(this).data( "src" );
+  //       arr = $src.split('/');
+  //       $vimeoVideoID = arr.slice(-1);
+
+  //       if($vimeoVideoID == data[0].id){
+  //         $(this).find(".thumbs").css('background-image', "url("+data[0].thumbnail_large+")");
+  //         console.log($(this).backgroundImage);
+  //       }
+  //     });
+  //   });
+  // });
+
+  // $.getJSON('https://www.vimeo.com/api/v2/video/' + $vimeoVideoID + '.json?callback=?', {format: "json"}, function(data) {  
+  //   $(".thumbs").attr('src', data[0].thumbnail_large);
+  // });
 
   var $videoSrc; 
   $('.video-btn').click(function() {
       $videoSrc = $(this).data( "src" );
   });
 
-  console.log($videoSrc);
   // when the modal is opened autoplay it  
   $('#myModal').on('shown.bs.modal', function (e) {
     // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
