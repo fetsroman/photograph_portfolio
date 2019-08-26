873f4ef3ac2246b9c9468e4e0d8f91ca6734389f
fetsroman <fets.roman@gmail.com>
2019-08-26 15:27:01 +0300

    Change video

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,30 +1,4 @@
 $(document).ready(function() {
-  // Gets the video src from the data-src on each button
-  
-  // $('.video-btn').each(function() {
-  //   $src = $(this).data( "src" );
-  //   arr = $src.split('/');
-  //   $vimeoVideoID = arr.slice(-1);
-    
-  //   $.getJSON('https://www.vimeo.com/api/v2/video/' + $vimeoVideoID + '.json?callback=?', {format: "json"}, function(data) {
-
-  //   $('.video-btn').each(function() {
-  //       $src = $(this).data( "src" );
-  //       arr = $src.split('/');
-  //       $vimeoVideoID = arr.slice(-1);
-
-  //       if($vimeoVideoID == data[0].id){
-  //         $(this).find(".thumbs").css('background-image', "url("+data[0].thumbnail_large+")");
-  //         console.log($(this).backgroundImage);
-  //       }
-  //     });
-  //   });
-  // });
-
-  // $.getJSON('https://www.vimeo.com/api/v2/video/' + $vimeoVideoID + '.json?callback=?', {format: "json"}, function(data) {  
-  //   $(".thumbs").attr('src', data[0].thumbnail_large);
-  // });
-
   var $videoSrc; 
   $('.video-btn').click(function() {
       $videoSrc = $(this).data( "src" );
