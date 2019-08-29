c66f1b37de78b28cc3bc557f949d0c79765770ee
Nor <nor8111@gmail.com>
2019-08-29 23:53:21 +0300

    updated video, centered it, fixed outline and popup

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,6 +15,16 @@ $(document).ready(function() {
     // a poor man's stop video
     $("#video").attr('src',$videoSrc); 
   }) 
-  // document ready  
+  // document ready
+
 });
-  
+
+function vh(v) {
+    const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (v * h) / 100;
+}
+
+function vw(v) {
+    const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    return (v * w) / 100;
+}
